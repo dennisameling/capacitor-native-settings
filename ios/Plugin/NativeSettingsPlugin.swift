11b87cc39af261794f4c2a800149a446c5286a17
Tăng Thanh tâm <trieutulong89kg@yahoo.com>
2021-07-05 14:07:23 +0700

    Fix issue related to Main Thread Checker: UI API called on a background thread: -[UIApplication applicationState]
    https://stackoverflow.com/a/45913510/1979190

--- ios/Plugin/NativeSettingsPlugin.swift
+++ ios/Plugin/NativeSettingsPlugin.swift
@@ -8,26 +8,28 @@ import Capacitor
 @objc(NativeSettingsPlugin)
 public class NativeSettingsPlugin: CAPPlugin {
     @objc func openIOS(_ call: CAPPluginCall) {
-        let value = call.getString("option") ?? ""
-        var settingsUrl:URL!
-        
-        if (value == "general") {
-            settingsUrl = URL(string: "App-Prefs:root=General")
-        } else if (value == "app") {
-            settingsUrl = URL(string: UIApplication.openSettingsURLString)
-        } else {
-            call.reject("Requested setting \"" + value + "\" is not available on iOS.");
-            return
-        }
-
-        if UIApplication.shared.canOpenURL(settingsUrl) {
-            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
-                call.resolve([
-                    "status": success
-                ])
-            })
-        } else {
-            call.reject("Cannot open settings")
+        DispatchQueue.main.async {
+            let value = call.getString("option") ?? ""
+            var settingsUrl:URL!
+            
+            if (value == "general") {
+                settingsUrl = URL(string: "App-Prefs:root=General")
+            } else if (value == "app") {
+                settingsUrl = URL(string: UIApplication.openSettingsURLString)
+            } else {
+                call.reject("Requested setting \"" + value + "\" is not available on iOS.");
+                return
+            }
+            
+            if UIApplication.shared.canOpenURL(settingsUrl) {
+                UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    call.resolve([
+                        "status": success
+                    ])
+                })
+            } else {
+                call.reject("Cannot open settings")
+            }
         }
     }
 }
